876dca0122d6832ace138b6faec93fcd8191e9b6
Alexander Abou Chedid <alexander.abouchedid@lau.edu>
2022-04-20 12:41:00 +0300

    server

---
+++ server/db_info.php
@@ -0,0 +1,16 @@
+<?php
+
+$db_host="localhost";
+$db_user="root";
+$db_pass= "123";
+$db_name="mobiledb";
+
+$mysqli = new mysqli($db_host,$db_user,$db_pass,$db_name);
+
+if(mysqli_connect_errno()){
+    die("Connection Failed" .$mysqli->connect_error);
+}
+
+
+
+?>
