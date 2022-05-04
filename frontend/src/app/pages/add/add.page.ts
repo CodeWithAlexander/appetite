12c7f9f3a96051e7c25dc58c02df922f390a071b
Alexander Abou Chedid <alexander.abouchedid@lau.edu>
2022-05-05 01:21:58 +0300

    so_much_work

--- frontend/src/app/pages/add/add.page.ts
+++ frontend/src/app/pages/add/add.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Camera,CameraResultType } from '@capacitor/camera';
 @Component({
   selector: 'app-add',
   templateUrl: './add.page.html',
   styleUrls: ['./add.page.scss'],
 })
 export class AddPage implements OnInit {
+  picture: string;
+  convertedimage: string;
 
   constructor() { }
 
   ngOnInit() {
   }
+  async takePicture(){
+    const image=await Camera.getPhoto({
+      quality:100,
+      allowEditing: false,
+      resultType: CameraResultType.Base64
+    });
 
+    this.picture=image.base64String;
+    this.convertedimage= 'data:image/jpeg;base64,'+this.picture;
+        console.log(this.convertedimage);
+  }
 }
